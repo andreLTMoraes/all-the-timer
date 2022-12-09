5180b6789adc0489366dd12fafb4b784aa1ff63c
André Moraes <andre-altm@hotmail.com>
2022-12-09 18:12:42 -0300

    Work_Rest timer logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useRef} from 'react'
 import {PlayArrow, Pause, Stop} from '@mui/icons-material'
 import './App.css';
 import MyDrawer from "./components/Drawer"
 
 function App() {
-  const [sec, setSec] = useState(0);
-  const [timer, setTimer] = useState(0)
+  const [timer, setTimer] = useState(10)
+  const [chronometer, setChronometer] = useState({
+    timerName: 'Round 1',
+    timerCounter: 4,
+    timerIdx: 0,
+    restFlag: false
+  })
   const [isActive, setIsActive] = useState(false)
   const [isPaused, setIsPaused] = useState(false)
   const countRef = useRef(null)
+  const sequence = {
+    type: 'WorkRest',
+    workTime: 10,
+    restTime: 10,
+    workNames: [
+      'Round 1',
+      'Round 2',
+      'Round 3'
+    ],
+    restNames: [
+      'Rest 1',
+      'Rest 2'
+    ]
+  }
+  const {timerName} = chronometer
 
   const handleStart = () => {
     setIsActive(true)
     setIsPaused(true)
     countRef.current = setInterval(() => {
-      setTimer((timer) => timer +1)
+      setTimer((timer) => 
+        timer === 0 ?
+        hasNext() :
+        timer -1)  
     }, 1000)
+  }
+
+  const hasNext = () => {
+    setChronometer((chronometer) => (
+      chronometer.timerCounter === 0 ?
+      handleReset(true) :
+      chronometer.restFlag ?
+      configWorkTimer(chronometer) :
+      configRestTimer(chronometer)
+    ))
+  }
+
+  const configRestTimer = (c) => {
+    setTimer(sequence.restTime)
+    return(
+      {
+        ...c,
+        timerName: sequence.restNames[c.timerIdx],
+        timerCounter: c.timerCounter - 1,
+        timerIdx: c.timerIdx + 1,
+        restFlag: true
+      }
+    )
+  }
+
+  const configWorkTimer = (c) => {
+    setTimer(sequence.workTime)
+    return(
+      {
+        ...c,
+        timerName: sequence.workNames[c.timerIdx],
+        timerCounter: c.timerCounter - 1,
+        restFlag: false
+      }
+    )
   }
 
   const handlePause = () => {
@@ -26,15 +84,27 @@ function App() {
   const handleResume = () => {
     setIsPaused(true)
     countRef.current = setInterval(() => {
-      setTimer((timer) => timer + 1)
+      setTimer((timer) => timer - 1)
     }, 1000)
   }
 
-  const handleReset = () => {
+  const handleReset = (autoStop) => {                                                    
     clearInterval(countRef.current)
     setIsActive(false)
     setIsPaused(false)
-    setTimer(0)
+    setTimer(sequence.workTime)
+    const resetObject = {
+      timerName: 'Round 1',
+      timerCounter: 4,
+      restFlag: false,
+      timerIdx: 0
+    }
+
+    return (
+    autoStop ?
+    resetObject :
+    setChronometer(resetObject)
+    )
   }
 
   const formatTime = () => {
@@ -45,12 +115,6 @@ function App() {
 
     return `${getHours} : ${getMinutes} : ${getSeconds}`
   }
-  useEffect(() => {
-    let interval = null
-    interval = setInterval(() => {
-      setSec((sec) => sec + 1000);
-    }, 1000);
-  }, [])
 
 
   return (
@@ -60,7 +124,10 @@ function App() {
           <p>
             A chronometer for everything!
           </p>
-          <p>
+          <p className='timer'>
+            {timerName}
+          </p>
+          <p className='timer'>
             {formatTime()}
             {/*00:{("0" + Math.floor((sec / 60000) % 60)).slice(-2)}:{("0" + Math.floor((sec / 2000) % 60)).slice(-2)}*/}
           </p>
@@ -80,7 +147,7 @@ function App() {
                 </button>
               )
             }
-            <button onClick={handleReset}>
+            <button onClick={() => handleReset(false)}>
               <Stop fontSize='large' sx={{color: '#fff'}}/>
             </button>
           </div>
